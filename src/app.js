491d45d9f1fe8120bbbf05a82d2535cafbcb4745
shannoj <69207315+shannoj@users.noreply.github.com>
2024-02-11 10:38:57 +0100

    connected react and express

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const cors = require('cors');
 
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World! Its me, your first express app!')
 })
