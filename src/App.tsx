fcffa07f9edcc991600e46da866c0e4e9704d687
shannoj <69207315+shannoj@users.noreply.github.com>
2024-02-11 10:41:48 +0100

    fixed local host in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 //data will be the string we send from our server
 const apiCall = () => {
-  axios.get("http://localhost:8080").then((data) => {
+  axios.get("http://localhost:5000").then((data) => {
     //this console.log will be in our frontend console
     console.log(data);
   });
